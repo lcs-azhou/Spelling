c8904d84e0dd694bbdc48571e6e8b207b317342f
Ansheng Zhou <azhou@lcs.on.ca>
2025-03-28 09:06:29 -0400

    User can make a guess and feedback appear in the debug console.

--- Spelling/Views/QuizView.swift
+++ Spelling/Views/QuizView.swift
@@ -11,6 +11,9 @@ struct QuizView: View {
     
     // MARK: Stored properties
     @State var currentItem = itemsToSpell.randomElement()!
+
+    //    The user's guess
+    @State var userGuess = ""
     
     // MARK: Computed properties
     var body: some View {
@@ -19,8 +22,28 @@ struct QuizView: View {
             Image(currentItem.imageName)
                 .resizable()
                 .scaledToFit()
+            
+            //            Allow the user to input
+            TextField("Enter the name of the item", text: $userGuess)
+            
+            //       Make it possible to Check
+            Button {
+                checkGuess()
+            } label: {
+                Text("Submit")
+            }
+
         }
         
+    }
+    
+    //    Function(s)
+    func checkGuess(){
+        if userGuess == currentItem.word{
+            print("correct")
+        } else{
+            print("incorrect")
+        }
     }
 }
 
